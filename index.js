9e8bdb9f46c3d3e970c94d51c93389fc4dde69dd
Babithakumari <babithakumari120@gmail.com>
2023-05-30 19:46:46 +0530

    Ex 3.19 completed

--- index.js
+++ index.js
@@ -94,20 +94,9 @@ const generateId = () => {
 
 }
 // add a new resource
-app.post("/api/persons",(request,response) =>{
+app.post("/api/persons",(request,response, next) =>{
     
     const body = request.body
-
-    if(!body.name){
-        return response.status(400).json({
-            error:"name missing"
-        })
-    }
-    if(!body.number){
-        return response.status(400).json({
-            error:"number missing"
-        })
-    }
     
     const person = new Person({
         id: generateId(),
@@ -118,6 +107,7 @@ app.post("/api/persons",(request,response) =>{
     person.save().then(savedPerson => {
         response.json(savedPerson)
     })
+    .catch(error => next(error))
 
     
 })
@@ -143,7 +133,13 @@ const errorHandler = (error, request,response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
-      } 
+    } 
+
+    if (error.name === 'ValidationError') {
+        return response.status(400).send({ error: error.message })
+    } 
+
+    
     
       next(error)
 }
